bed7a4b3dba93af404f5efdb4efde16b85c49d8d
Abdelhadi Akroud <aakroud@e3r4p14.1337.ma>
2026-08-17 14:39:50 +0100

    maybe

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -56,6 +56,44 @@ PmergeMe(char **av, size_t size)
     }
 }
 
+std::vector<size_t> PmergeMe::generate_jacob(size_t m)
+{
+    std::vector<size_t> js;
+    js.push_back(1);
+    js.push_back(1);
+    size_t i = 1;
+    while (js[i] < m)
+    {
+        i++;
+        js.push_back(js[i-1]+2*js[i-2]);
+    }
+    return (js);
+}
+
+std::vector<size_t> PmergeMe::insert_order(size_t m)
+{
+    std::vector<size_t> o;
+    if (m < 2)
+        return (o); // needs explain
+    std::vector<size_t> js = generate_jacob(m);
+    size_t prev = 1;
+    size_t l = 0;
+    for (size_t i = 2; i < js.size(); i++)
+    {
+        l = std::min(js[i], m);
+        for (size_t j = l; j > prev; j--)
+            o.push_back(j);
+        prev = l;
+    }
+    return (o);
+}
+
+void binaryInsert(std::vector<int> &m, std::vector<int>::iterator b, int v)
+{
+    std::vector<int>::iterator it = std::lower_bound(m.begin(), b, v);
+    m.insert(it, v);
+}
+
 std::vector<int> PmergeMe::sorted(std::vector<int> &vect_cont)
 {
     if (vect_cont.size() == 1)
@@ -89,6 +127,38 @@ std::vector<int> PmergeMe::sorted(std::vector<int> &vect_cont)
     w = sorted(w);
     std::vector<int> m;
     std::vector<int> p;
+    m.push_back(w[0]);
+    // push the winner in main
+    for (size_t i = 0; i < ps.size(); i++)
+    {
+        if (ps[i].first == w[0])
+        {
+            m.insert(m.begin(), p[i].second);
+            break;
+        }
+    }
+    //push the loser of the winner in main
+    for (size_t i = 1; i < w.size(); i++)
+        m.push_back(w[i]);
+    //push every other winner
+    for (size_t i = 1; i < w.size(); i++)
+    {
+        for (size_t j = 0; j < ps.size(); j++)
+        {
+            if (ps[j].first == w[k])
+            {
+                p.push_back(ps[j].second);
+                break;
+            }
+        }
+    }
+    //insert pend with losers
+    std::vector<size_t> loser_order =  insert_order(w.size());
+    //the loser inserted order in the pend
+    for (size_t i = 0; i < loser-order.size(); i++)
+    {
+        
+    }
 }
 
 void PmergeMe::final_sort(std::vector<int> &vect_cont)
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -21,6 +21,8 @@ class PmergeMe
         PmergeMe(char **av, size_t size);
         std::vector<int> sorted(std::vector<int> &vect_cont);
         void final_sort();
+        std::vector<size_t> generate_jacob(size_t m);
+        std::vector<size_t> insert_order(size_t m);
 };
 
 #endif
